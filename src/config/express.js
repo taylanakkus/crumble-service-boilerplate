0379e34c3fef3405736eaa26ab7483e3baddb9d6
t4yo <taylan.akkus@outlook.com>
2020-05-22 17:35:15 +0300

    morgan set up. winston logs moved to log folder

--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-
+const morgan = require('morgan');
 const app = express();
 
+const {logs} = require('../config/vars');
+
 // apply middlewares
+app.use(morgan(logs));
+
 app.use(cors());
 app.use(express.json());
 
