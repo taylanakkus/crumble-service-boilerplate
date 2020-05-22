26127dbd45ddcbf1c19b5b31d64be15b220edeac
t4yo <taylan.akkus@outlook.com>
2020-05-22 17:01:13 +0300

    logger now available

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const app = require('./config/express');
-
+const logger = require('./config/logger');
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is listening on: ${port}`);
+  logger.info(`server started on port ${port}`);
 });
